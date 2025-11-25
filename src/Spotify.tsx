73ccfcf4b02a367b67e5773f72ea2d455af09d00
bmisz <benmisner26@gmail.com>
2025-11-24 21:12:09 -0500

    Add more assets to weather widget

--- src/Spotify.tsx
+++ src/Spotify.tsx
@@ -2,94 +2,11 @@ import './Spotify.css';
 import skip from './assets/skip-button.svg';
 import spotify from './assets/spotify-logo.svg';
 import pause from './assets/pause.svg';
-import { useEffect, useRef } from 'react';
+
+//TODO do this idk if i ever wanna make a spotify one.
 export default function Spotify() {
-	const playerRef = useRef<any>(null);
-	//FIXME I dont actually need any of this i was using sdk instead of web api
-	function addSpotifyListeners(player: any) {
-		// Ready
-		player.addListener('ready', ({ device_id }: { device_id: string }) => {
-			console.log('Ready with Device ID', device_id);
-		});
 
-		// Not Ready
-		player.addListener(
-			'not_ready',
-			({ device_id }: { device_id: string }) => {
-				console.log('Device ID has gone offline', device_id);
-			}
-		);
-		player.addListener(
-			'initialization_error',
-			({ message }: { message: any }) => {
-				console.error(message);
-			}
-		);
-
-		player.addListener(
-			'authentication_error',
-			({ message }: { message: any }) => {
-				console.error(message);
-			}
-		);
-
-		player.addListener('account_error', ({ message }: { message: any }) => {
-			console.error(message);
-		});
-	}
-	useEffect(() => {
-		console.log('mount');
-		(window as any).onSpotifyWebPlaybackSDKReady = () => {
-			console.log('Spotify SDK is ready');
-			const token = `${import.meta.env.VITE_SPOTIFY_ACCESS_TOKEN}`;
-
-			if (!token) {
-				console.error('No Spotify token found');
-				return;
-			}
-
-			const player = new (window as any).Spotify.Player({
-				name: 'Web Playback SDK Quick Start Player',
-				getOAuthToken: (cb: any) => {
-					cb(token);
-				},
-				volume: 0.5,
-			});
-
-			playerRef.current = player;
-			addSpotifyListeners(player);
-
-			player.connect().then((success: boolean) => {
-				if (success) {
-					console.log('Player connected successfully');
-				} else {
-					console.error('Player connection failed');
-				}
-			});
-		};
-
-		// Check if SDK already loaded
-		if ((window as any).Spotify) {
-			console.log('Spotify SDK already loaded, initializing...');
-			(window as any).onSpotifyWebPlaybackSDKReady();
-		}
-
-		return () => {
-			if (playerRef.current) {
-				playerRef.current.disconnect();
-			}
-		};
-	}, []);
-
-	function togglePlay() {
-		if (playerRef.current) {
-			playerRef.current.togglePlay();
-			console.log('Toggle play clicked.');
-		} else {
-			console.log('Player not initialized.');
-		}
-	}
-
+	
 	return (
 		<div className="spotify-widget">
 			<div className="album-cover">Album Cover</div>
@@ -102,7 +19,7 @@ export default function Spotify() {
 					<img
 						className="media media--pause"
 						src={pause}
-						onClick={togglePlay}
+						// onClick={togglePlay}
 					/>
 					<img className="media media--right" src={skip} />
 				</div>
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,37 @@
 import clear from './clear.svg';
 import partlycloudy from './partlycloudy.svg';
 import cloudy from './cloudy.svg';
+import lightsnow from './light-snow.svg';
+import snow from './snow.svg';
+import heavysnow from './heavy-snow.svg';
+import lightrain from './light-rain.svg';
+import rain from './rain.svg';
+import heavyrain from './heavy-rain.svg';
+import extremerain from './extreme-rain.svg';
+import sleet from './sleet.svg';
 
 export const weatherIcons: Record<number, string> = {
+	300: lightrain,
+	301: lightrain,
+	302: lightrain,
+	310: lightrain,
+	311: lightrain,
+	312: lightrain,
+	313: lightrain,
+	314: lightrain,
+	321: lightrain, 
+	500: lightrain,
+	501: rain,
+	502: heavyrain,
+	503: heavyrain,
+	504: extremerain,
+	600: lightsnow,
+	601: snow,
+	602: heavysnow,
+	611: sleet,
+	620: lightsnow,
+	621: snow,
+	622: heavysnow,
 	800: clear,
 	802: partlycloudy,
 	803: cloudy,
